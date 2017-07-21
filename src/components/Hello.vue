dfd90f0749500e5a50b6522cacbc16e26f6dc0ec
PedroManriquez <ptrmanriquez@gmail.com>
2017-07-21 13:28:23 -0400

    Creado scaffold con vue-cli, añadido element-ui

---
+++ src/components/Hello.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <el-button type="success" @click="open">Click to open the Message Box</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  methods: {
+    open: function () {
+      this.$alert('This is a message', 'Title', {
+        confirmButtonText: 'OK',
+        callback: action => {
+          this.$message({
+            type: 'info',
+            message: `action: ${action}`
+          })
+        }
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/es'
+
+Vue.config.productionTip = false
+
+Vue.use(ElementUI)
+Vue.use(locale)
+Vue.use(ElementUI.Loading.directive)
+
+Vue.prototype.$loading = ElementUI.Loading.service
+Vue.prototype.$msgbox = ElementUI.MessageBox
+Vue.prototype.$alert = ElementUI.MessageBox.alert
+Vue.prototype.$confirm = ElementUI.MessageBox.confirm
+Vue.prototype.$prompt = ElementUI.MessageBox.prompt
+Vue.prototype.$notify = ElementUI.Notification
+Vue.prototype.$message = ElementUI.Message
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
